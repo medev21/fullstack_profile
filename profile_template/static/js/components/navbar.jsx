73ab4efb761c4cf0c1bfd8f9cf3282965cf94d2b
medev21 <benmarteen07@gmail.com>
2018-08-02 16:55:26 -0400

    save work - passing experience in profile item - value not showing up

--- profile_template/static/js/components/navbar.jsx
+++ profile_template/static/js/components/navbar.jsx
@@ -7,57 +7,177 @@ class Navbar extends Component {
 	constructor(props){
 		super(props);
 		this.state = {
-			profile: []
+			// profile: [
+			// 	{'experience': [
+			// 		{
+			// 			'company': 'company A',
+			// 			'date': 'August 2016 - Present',
+			// 			'jobTitle': 'salesman'
+			// 		},
+			// 		{
+			// 			'company': 'company B',
+			// 			'date': 'February 2016 - August 2016',
+			// 			'jobTitle': 'Developer'
+			// 		},
+			// 		{
+			// 			'company': 'company C',
+			// 			'date': 'August 2012 - October 2015',
+			// 			'jobTitle': 'clerk'
+			// 		}
+			// 	]},
+			// 	{'education': [
+			// 		{
+			// 			'school': 'shcool A',
+			// 			'date': 'Fall 2015',
+			// 			'degree': 'mathematics'
+			// 		},
+			// 		{
+			// 			'school': 'school B',
+			// 			'date': 'May 2008',
+			// 			'degree': 'business'
+			// 		}
+			// 	]},
+			// 	{'skills': [
+			// 		{
+			// 			'Tools': 'HTML, CSS, Javascript, SQL, Python'
+			// 		}
+			// 	]}
+			// ],
+			experience: [
+				{
+					'company': 'company A',
+					'date': 'August 2016 - Present',
+					'jobTitle': 'salesman'
+				},
+				{
+					'company': 'company B',
+					'date': 'February 2016 - August 2016',
+					'jobTitle': 'Developer'
+				},
+				{
+					'company': 'company C',
+					'date': 'August 2012 - October 2015',
+					'jobTitle': 'clerk'
+				}
+			],
+			education: [
+				{
+					'school': 'shcool A',
+					'date': 'Fall 2015',
+					'degree': 'mathematics'
+				},
+				{
+					'school': 'school B',
+					'date': 'May 2008',
+				}
+			],
+			skills: [
+				{
+					'Tools': 'HTML, CSS, Javascript, SQL, Python'
+				}
+			]
 		};
 	}
 
-	getProfile(){
-		this.setState(
-			{
-				profile: [
-					{'experience': [
-						{
-							'company': 'Groundtruth',
-							'date': 'August 2016 - Present',
-							'jobTitle': 'Software Engineer'
-						},
-						{
-							'company': 'Learning Library TV',
-							'date': 'February 2016 - August 2016',
-							'jobTitle': 'Junior Developer'
-						},
-						{
-							'company': 'Smelter Service',
-							'date': 'August 2012 - October 2015',
-							'jobTitle': 'Plant Engineer'
-						}
-					]},
-					{'education': [
-						{
-							'school': 'Startup Institute',
-							'date': 'Fall 2015 Cohort',
-							'degree': 'Web Development Coursework'
-						},
-						{
-							'school': 'Tennessee Tech University',
-							'date': 'May 2012',
-							'degree': 'Bachelor of Science, Mechanical Engineering'
-						}
-					]},
-					{'skills': [
-						{
-							'Tools': 'HTML, CSS, Javascript, SQL, Python, Autocad, SolidWorks'
-						}
-					]}
-				]
-			}
-		);
-	}
+	// getProfile(){
+	// 	this.setState(
+	// 		{
+	// 			profile: [
+	// 				{'experience': [
+	// 					{
+	// 						'company': 'company A',
+	// 						'date': 'August 2016 - Present',
+	// 						'jobTitle': 'salesman'
+	// 					},
+	// 					{
+	// 						'company': 'company B',
+	// 						'date': 'February 2016 - August 2016',
+	// 						'jobTitle': 'Developer'
+	// 					},
+	// 					{
+	// 						'company': 'company C',
+	// 						'date': 'August 2012 - October 2015',
+	// 						'jobTitle': 'clerk'
+	// 					}
+	// 				]},
+	// 				{'education': [
+	// 					{
+	// 						'school': 'shcool A',
+	// 						'date': 'Fall 2015',
+	// 						'degree': 'mathematics'
+	// 					},
+	// 					{
+	// 						'school': 'school B',
+	// 						'date': 'May 2008',
+	// 						'degree': 'business'
+	// 					}
+	// 				]},
+	// 				{'skills': [
+	// 					{
+	// 						'Tools': 'HTML, CSS, Javascript, SQL, Python'
+	// 					}
+	// 				]}
+	// 			]
+	// 		}
+	// 	);
+	// }
+
+	// getExperience(){
+	// 	this.setState(
+	// 		{'experience': [
+	// 			{
+	// 				'company': 'Groundtruth',
+	// 				'date': 'August 2016 - Present',
+	// 				'jobTitle': 'Software Engineer'
+	// 			},
+	// 			{
+	// 				'company': 'Learning Library TV',
+	// 				'date': 'February 2016 - August 2016',
+	// 				'jobTitle': 'Junior Developer'
+	// 			},
+	// 			{
+	// 				'company': 'Smelter Service',
+	// 				'date': 'August 2012 - October 2015',
+	// 				'jobTitle': 'Plant Engineer'
+	// 			}
+	// 		]}
+	// 	);
+	// }
+
+	// getEducation(){
+	// 	this.setState(
+	// 		{'education': [
+	// 			{
+	// 				'school': 'Startup Institute',
+	// 				'date': 'Fall 2015 Cohort',
+	// 				'degree': 'Web Development Coursework'
+	// 			},
+	// 			{
+	// 				'school': 'Tennessee Tech University',
+	// 				'date': 'May 2012',
+	// 				'degree': 'Bachelor of Science, Mechanical Engineering'
+	// 			}
+	// 		]},
+	// 	);
+	// }
+
+	// getSkills(){
+	// 	this.setState(
+	// 		{'skills': [
+	// 			{
+	// 				'Tools': 'HTML, CSS, Javascript, SQL, Python, Autocad, SolidWorks'
+	// 			}
+	// 		]}
+	// 	);
+	// }
 
 
-	componentDidMount(){
-		this.getProfile();
-		console.log(this.state.profile);
+	componentWillMount(){
+		// this.getProfile();
+		// this.getExperience();
+		// this.getEducation();
+		// this.getSkills();
+		// console.log(this.state.profile);
 	}
 
 	render(){
@@ -76,7 +196,7 @@ class Navbar extends Component {
 					<div className="bottomNav"><div className="navBar"><h3>Contact</h3></div></div>
 				</Link>
 
-				<Link to={{pathname: '/profile', profile: this.state.profile}}>
+				<Link to={{pathname: '/profile', state: {experience: this.state.experience, education: this.state.education, skills: this.state.skills}}}>
 					<div className="leftNav"><div className="navBar"><h3>Profile</h3></div></div>
 				</Link>
 	      	</div>
--- profile_template/static/js/components/profilePage.jsx
+++ profile_template/static/js/components/profilePage.jsx
@@ -5,58 +5,92 @@ class Profile extends Component {
 
 	render(){
 
-		let profile = this.props.location.profile.map((experience) => <li>{experience.experience}</li>);
-		// let profile = this.props.location.profile;
-		// console.log(this.props.location.profile);
-		console.log(profile);
-		return(
-			<div className="introWrapper">
-				<div className="experienceSection">
-					<h3>Experience</h3>
+		const experiences = this.props.location.state.experience;
+		let educations = this.props.location.state.education;
+		let skills = this.props.location.state.skills;
+		// console.log(experiences);
+		// console.log(education);
+		// console.log(skills);
+		return (experiences.map((experience, idx) => {
+
+			
+
+			// <ProfileItem key={idx} experience={experience}/>
+				<div className="introWrapper">
 					<ul>
-						<li>
-							<h4>Company A</h4>
-							<p>april 2020 - present</p>
-							<p>engineer</p>
-						</li>
-						<li>
-							<h4>Company A</h4>
-							<p>april 2020 - present</p>
-							<p>engineer</p>
-						</li>
-						<li>
-							<h4>Company A</h4>
-							<p>april 2020 - present</p>
-							<p>engineer</p>
-						</li>
+						<li key={experience.company}>{experience.company}</li>
+						<li key={experience.date}>{experience.date}</li>
+						<li key={experience.jobTitle}>{experience.jobTitle}</li>
 					</ul>
 				</div>
-				<div className="educationSection">
-					<h3>Education</h3>
-					<ul>
-						<li>
-							<h4>School A</h4>
-							<p>april 2020 - present</p>
-							<p>engineer</p>
-						</li>
-						<li>
-							<h4>School A</h4>
-							<p>april 2020 - present</p>
-							<p>engineer</p>
-						</li>
-						<li>
-							<h4>School A</h4>
-							<p>april 2020 - present</p>
-							<p>engineer</p>
-						</li>
-					</ul>
-				</div>
-				<div className="skillSection">
-					<h3>Skills</h3>
-					<p>HTML, CSS, JAVASCRIPT</p>
-				</div>
-			</div>
-		)
+
+			console.log(experience.company)
+			console.log(experience.date)
+			console.log(experience.jobTitle)
+				
+			
+		}));
+
+
+
+
+		// const experience = this.props.location.profile.map((profile,idx) => {
+		// 	console.log(profile.experience, 'passing');
+		// 	return profile[profile].experience.map((experience,idx) => 
+		// 			<div>
+		// 				<p key={idx}>{experience.company}</p>
+		// 			</div>
+		// 		);
+		// 	});
+
+		// return(
+		// profile.map((bio, idx) => {
+		// 	console.log(bio);
+		// 	console.log(idx);
+		// 	bio[idx].map((item, idx) => {
+		// 		console.log(item, 'this is the item');
+		// 		<p key={idx}>{bio.company}</p>	
+		// 	})
+
+		// }));
+
+		// profile.map((bio, idx) => {
+		// 	return(
+		// 		<h1>bio</h1>
+		// 	);
+		// });
+
+
+		// if(this.props.location.profile){
+		// 	const experience = this.props.location.profile.map((bio,idx) => {
+		// 	console.log(bio.experience, 'passing');
+
+		// 	if(bio.experience){
+		// 		return this.props.location.profile[bio].map((company,idx) => 
+		// 			<div>
+		// 				<p key={idx}>{company}</p>
+		// 			</div>
+		// 		);
+		// 	}});
+			
+		// }
+
+		// return(
+		// 	this.props.location.profile.map((profile, idx) => {
+		// 		profile.map((experience, idx) => 
+		// 			<p key={idx}>{experience.company}</p>	
+		// 		);	
+		// 	})
+		// )
+
+
+		// return(
+		// 	this.props.location.profile.map((profile, idx) => {
+		// 		profile.map((experience, idx) => 
+		// 			<p key={idx}>{experience.company}</p>	
+		// 		);	
+		// 	})
+		// )
 	}
 }
 
