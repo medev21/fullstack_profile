7c86c173214a834e35c6d74025a03c07e29d2f87
medev21 <benmarteen07@gmail.com>
2018-08-04 15:27:06 -0400

    add work design html in work page

--- profile_template/static/js/components/projectsPage.jsx
+++ profile_template/static/js/components/projectsPage.jsx
@@ -4,7 +4,27 @@ class Projects extends Component {
 	render(){
 		return(
 			<div className="introWrapper">
-				<h3>experience page</h3>
+				<h3>Projects</h3>
+				<div>
+					<a>
+						<div><h4>kanban board</h4></div>
+						<div><p>a trello clone app</p></div>
+					</a>
+				</div>
+
+				<div>
+					<a>
+						<div><h4>worlcups</h4></div>
+						<div><p>a list of all the worlcups</p></div>
+					</a>
+				</div>
+
+				<div>
+					<a>
+						<div><h4>commcx</h4></div>
+						<div><p>online learning platform</p></div>
+					</a>
+				</div>
 			</div>
 		)
 	}
