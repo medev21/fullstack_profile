41987cc65e3d7da5ccae384ee1eaa0d1f18891cf
medev21 <benmarteen07@gmail.com>
2018-08-07 10:01:16 -0400

    added react-fontawesome for social media

--- profile_template/static/js/components/contactPage.jsx
+++ profile_template/static/js/components/contactPage.jsx
@@ -1,10 +1,22 @@
 import React, { Component } from "react";
+import FontAwesome from "react-fontawesome";
 
 class Contact extends Component {
 	render(){
 		return(
 			<div className="introWrapper">
-				<h3>Contact page</h3>
+				<div className="contactHeader">
+					<h2>Let's stay in touch</h2>
+					<p>I hope you learned a bit about me.  Need a developer for you projects?  Feel free to say Hi!</p>
+					<a href="mailto:martindevelops21@gmail.com?subject=Comments" title="Email me!">Email</a>
+				</div>
+
+				<div className="socialMedia">
+					<ul>
+						<li><FontAwesome name='linkedin' size="2x"/></li>
+						<li><FontAwesome name='github' size="2x"/></li>
+					</ul>
+				</div>
 			</div>
 		)
 	}
