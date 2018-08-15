8c9f673a6a14e3f120ccad70d85124fed20c5f0c
medev21 <benmarteen07@gmail.com>
2018-08-15 10:17:17 -0400

    add email icon

--- profile_template/static/js/components/contactPage.jsx
+++ profile_template/static/js/components/contactPage.jsx
@@ -8,11 +8,11 @@ class Contact extends Component {
 				<div className="contactHeader">
 					<h2>Let's stay in touch</h2>
 					<p>I hope you learned a bit about me.  Need a developer for you projects?  Feel free to say Hi!</p>
-					<a href="mailto:martindevelops21@gmail.com?subject=Comments" title="Email me!">Email</a>
 				</div>
 
 				<div className="socialMedia">
 					<ul>
+						<li><FontAwesome name='envelope' size="2x"/><a href="mailto:martindevelops21@gmail.com?subject=Comments" title="Email me!"></a></li>
 						<li><FontAwesome name='linkedin' size="2x"/></li>
 						<li><FontAwesome name='github' size="2x"/></li>
 					</ul>
