aac52ac0a5aa88d9cf4f01c19dfba1e2795b7293
medev21 <benmarteen07@gmail.com>
2018-09-03 15:50:57 -0400

    refactor css in navbar

--- profile_template/static/js/components/navbar.jsx
+++ profile_template/static/js/components/navbar.jsx
@@ -7,43 +7,49 @@ class Navbar extends Component {
 	render(){
 		return (
 			<div className="navbarContainer">
-			<div className="navFrame">
-				<Link to="/">
-					<div className="topNav"><div className="navBar"><h3>Martin Benavides</h3></div></div>
-				</Link>
+				<div className="navFrame">
+					<Link to="/">
+						<div className="topNav"><div className="navBar"><h3>Martin Benavides</h3></div></div>
+					</Link>
 
-				<Link to="/projects" >
-					<div className="rightNav"><div className="navBar"><h3>Projects</h3></div></div>
-				</Link>
+					<Link to="/projects" >
+						<div className="rightNav"><div className="navBar"><h3>Projects</h3></div></div>
+					</Link>
 
-				<Link to="/contact">
-					<div className="bottomNav"><div className="navBar"><h3>Contact</h3></div></div>
-				</Link>
+					<Link to="/contact">
+						<div className="bottomNav"><div className="navBar"><h3>Contact</h3></div></div>
+					</Link>
 
-				<Link to={{pathname: '/profile'}}>
-					<div className="leftNav"><div className="navBar"><h3>Profile</h3></div></div>
-				</Link>
-	      	</div>
+					<Link to={{pathname: '/profile'}}>
+						<div className="leftNav"><div className="navBar"><h3>Profile</h3></div></div>
+					</Link>
+		      	</div>
 
-	      	<div className="mobileNavFrame">
-	      		<Link to="/">
-					<div className="topNav"><div className="navBar"><h3>Martin Benavides</h3></div></div>
-				</Link>
+		      	<div className="mobileNavFrame">
+		      		<Link to="/">
+						<div className="topNav"><div className="navBar"><h3>Martin Benavides</h3></div></div>
+					</Link>
 
-				
+					<div id="nav-icon3">
+					  <span></span>
+					  <span></span>
+					  <span></span>
+					  <span></span>
+					</div>
+					
 
-				<Link to="/contact">
-					<div className="bottomNav"><div className="navBar"><h3>Contact</h3></div></div>
-				</Link>
+					<Link to="/contact">
+						<div className="bottomNav"><div className="navBar"><h3>Contact</h3></div></div>
+					</Link>
 
-				{/*<Link to="/projects" >
-					<div className="rightNav"><div className="navBar"><h3>Projects</h3></div></div>
-				</Link>*/}
+					{/*<Link to="/projects" >
+						<div className="rightNav"><div className="navBar"><h3>Projects</h3></div></div>
+					</Link>*/}
 
-				{/*<Link to={{pathname: '/profile'}}>
-					<div className="leftNav"><div className="navBar"><h3>Profile</h3></div></div>
-				</Link>*/}
-	      	</div>
+					{/*<Link to={{pathname: '/profile'}}>
+						<div className="leftNav"><div className="navBar"><h3>Profile</h3></div></div>
+					</Link>*/}
+		      	</div>
 	      	</div>
 
 		);
