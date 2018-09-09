f1877dcca5a016434d4ffbea8e1631dae50bc16b
medev21 <benmarteen07@gmail.com>
2018-09-09 16:19:15 -0400

    add lone tree video, css styling

--- profile_template/static/js/components/introPage.jsx
+++ profile_template/static/js/components/introPage.jsx
@@ -1,19 +1,26 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import video from "../../videos/lake.mp4"
+import video2 from "../../videos/lonetree.mp4"
 
 class Intro extends Component {
 	render(){
 
 		return(
-			<div className="defaultWrapper homeWrapper">
-				{/*<video loop autoPlay>
-					<source src={video} type="video/mp4" />
+			<div>
+				<video className="introVid" loop autoPlay>
+					<source src={video2} type="video/mp4" />
 					Your browser does not support the video tag.
-				</video>*/}
-				<h2>Frontend developer who enjoys finding solutions to complex problems in an elegant way.</h2>
-				<p>Enthusiastic, motivated, and high-performing team player who is passionate about frontend development</p>
-				<Link to='/profile'>View profile</Link>
+				</video>
+				<div className="defaultWrapper homeWrapper">
+					{/*<video loop autoPlay>
+						<source src={video} type="video/mp4" />
+						Your browser does not support the video tag.
+					</video>*/}
+					<h2>Frontend developer who enjoys finding solutions to complex problems in an elegant way.</h2>
+					<p>Enthusiastic, motivated, and high-performing team player who is passionate about frontend development</p>
+					<Link to='/profile'>View profile</Link>
+				</div>
 			</div>
 		)
 	}
