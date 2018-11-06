4d8580e40d0ca43ec0b7486261d1e82160336431
medev21 <benmarteen07@gmail.com>
2018-11-06 10:38:29 -0500

    add weather scan route in projectsPage.jsx

--- profile_template/static/js/components/projectsPage.jsx
+++ profile_template/static/js/components/projectsPage.jsx
@@ -4,6 +4,7 @@ import ProjectNavbar from "./projectsNavbar";
 import Kanban from "./kanbanPage";
 import Worldcups from "./worldcupsPage";
 import Commcx from "./commcxPage";
+import WeatherScan from "./weatherScanPage";
 import ScrollToTopOnMount from "./scrollToTop";
 
 class Projects extends Component {
@@ -22,6 +23,7 @@ class Projects extends Component {
 				<Route path={`${url}/kanban_board`} component={Kanban}/>
 				<Route path={`${url}/worldcups`} component={Worldcups}/>
 				<Route path={`${url}/commcx`} component={Commcx}/>
+				<Route path={`${url}/weatherscan`} component={WeatherScan}/>
 				<Route exact path={url}/>
 			</div>
 		)
