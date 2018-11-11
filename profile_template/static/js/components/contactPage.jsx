d49c581a6212a8ae119e7791b68dad2e58f6231a
medev21 <benmarteen07@gmail.com>
2018-11-11 12:20:48 -0500

    add form in contact page with formspree

--- profile_template/static/js/components/contactPage.jsx
+++ profile_template/static/js/components/contactPage.jsx
@@ -12,12 +12,33 @@ class Contact extends Component {
 					<p>I hope you learned a bit about me.  Need a developer for your projects?  Feel free to say hi!</p>
 				</div>
 
-				<div className="socialMedia">
-					<ul>
-						<li><a href="mailto:benmarteen07@gmail.com?subject=Comments" title="Email me!"><FontAwesome name='envelope' size="4x"/></a></li>
-						<li><a href="https://www.linkedin.com/in/medev21/" target="_blank"><FontAwesome name='linkedin' size="4x"/></a></li>
-						<li><a href="https://github.com/medev21" target="_blank"><FontAwesome name='github' size="4x"/></a></li>
-					</ul>
+				<div className="contactBody">
+					<div className="formSection">
+						<form action="https://formspree.io/benmarteen07@gmail.com" method="POST">
+							<div className="nameRow">
+								<div className="nameColumn formColumn">
+									<input className="inputName" type="text" id="fname" name="name" placeholder="Name"/>
+								</div>
+								<div className="emailColumn formColumn">
+									<input className="inputEmail" type="email" id="useremail" name="_replyto" placeholder="Email"/>
+								</div>
+							</div>
+							<div className="textRow">
+								<textarea name="message" id="usermessage" cols="10" rows="4" placeholder="Your message..."></textarea>
+							</div>
+							<div className="btnRow">
+								<button type="submit" value="Send">Send</button>
+							</div>
+						</form>
+					</div>
+
+					<div className="socialMedia">
+						<ul>
+							<li><a href="mailto:benmarteen07@gmail.com?subject=Comments" title="Email me!"><FontAwesome name='envelope' size="4x"/></a></li>
+							<li><a href="https://www.linkedin.com/in/medev21/" target="_blank"><FontAwesome name='linkedin' size="4x"/></a></li>
+							<li><a href="https://github.com/medev21" target="_blank"><FontAwesome name='github' size="4x"/></a></li>
+						</ul>
+					</div>
 				</div>
 			</div>
 		)
