cdb8250b3ec639864c4c1b840c353d8d7011301f
medev21 <benmarteen07@gmail.com>
2025-05-23 23:42:03 -0400

    create profile page with resumve svg

--- src/Pages/ProfilePage.tsx
+++ src/Pages/ProfilePage.tsx
@@ -1,7 +1,29 @@
+// ASSSETS
+import resume from '../assets/resume.svg'
+// DEPENDENCIES
+import Box from '@mui/material/Box'
 
 function ProfilePage() {
     return(
-        <div>Profile page</div>
+        <Box
+            component={'main'}
+            sx={{
+                overflow: 'auto',
+                paddingX: 5,
+            }}
+        >
+
+            <Box
+                component="img"
+                src={resume}
+                alt="My Resume"
+                sx={{
+                    display: 'block',  // removes inline‐img whitespace
+                    width: '100%',     // scale to full width of frame
+                    height: 'auto',    // preserve aspect ratio & natural height
+                }}
+            />
+        </Box>
     )
 }
 
