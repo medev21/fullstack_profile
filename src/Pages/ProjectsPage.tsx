a10a9af03518fc6cde81ee6368ad6a73ee980036
medev21 <benmarteen07@gmail.com>
2025-06-14 16:30:58 -0400

    add project links

--- src/Pages/ProjectsPage.tsx
+++ src/Pages/ProjectsPage.tsx
@@ -1,15 +1,62 @@
-// COMPONENTS
-import { ComingSoon } from 'src/components/ComingSoon';
 // DEPENDENCIES
+import ArrowOutwardIcon from '@mui/icons-material/ArrowOutward';
 import Box from '@mui/material/Box';
+import List from '@mui/material/List';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+
+interface ProjectProps {
+    label: string;
+    href: string;
+}
+
+const projects: ProjectProps[] = [
+    {
+        label: 'weather app',
+        href: 'https://medev21.github.io/weatherApp/'
+    },
+    {
+        label: 'material UI components storybook',
+        href: 'https://medev21.github.io/MUIReactComponents/',
+    }
+]
 
 function ProjectsPage() {
     return(
         <Box
             component={'main'}
+            display={'flex'}
+            alignItems={'center'}
+            justifyContent={'center'}
+            flexDirection={'column'}
             height={'100%'}
         >
-            <ComingSoon />
+            <List>
+                {projects.map((item) => (
+                    <ListItemButton
+                        href={item.href}
+                        target='_blank'
+                        disableRipple
+                    >
+                        <ListItemText
+                            sx={{
+                                mr: 8,
+                                textTransform: 'capitalize'
+                            }}
+                            primary={item.label}
+                        />
+                        <ListItemIcon
+                            sx={{
+                                alignItems: 'center',
+                                minWidth: 0
+                            }}
+                        >
+                            <ArrowOutwardIcon htmlColor='white'/>
+                        </ListItemIcon>
+                    </ListItemButton>
+                ))}
+            </List>
         </Box>
     )
 }
